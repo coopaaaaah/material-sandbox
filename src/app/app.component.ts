383d1ed125b675be037cfc783d4746a7c7606d9e
David Cooper <dmcooper2412@gmail.com>
2017-03-30 19:38:23 -0400

    Sandbox project for message log design using material 2 framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MdDialog, MdDialogRef} from "@angular/material";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  messages = [
+    {
+      text: 'One',
+      type: 'ASN',
+      isSelected: false,
+      isError: false,
+      cols: 1,
+      rows: 1
+    },
+    {
+      text: 'One',
+      type: 'Sku',
+      isSelected: false,
+      isError: false,
+      cols: 1,
+      rows: 1
+    },
+    {
+      text: 'One',
+      type: 'Distro',
+      isSelected: false,
+      isError: false,
+      cols: 1,
+      rows: 1
+    }
+  ];
+
+  selected = [];
+
+  constructor(public dialog: MdDialog){}
+
+  openDialog(message: any) {
+    let dialogRef = this.dialog.open(DialogResultExampleDialog);
+
+    dialogRef.componentInstance.message = message;
+
+    dialogRef.afterClosed().subscribe(result => {
+
+    });
+  }
+
+  toggleHover(message: any){
+    message.isHovered = !message.isHovered;
+  }
+
+  toggleSelection(message: any) {
+    message.isSelected = !message.isSelected;
+    this.filterSelection();
+  }
+
+  private filterSelection() {
+    this.selected = this.messages.filter(
+      message => message.isSelected
+    );
+  }
+}
+
+@Component({
+  selector: 'dialog-result-example-dialog',
+  template: `
+    <h1>Dialog Box</h1>
+    <p>{{message.type}}</p>
+    <p>{{message.text}}</p>
+    <button md-button md-dialog-close>close</button>
+    `,
+})
+export class DialogResultExampleDialog {
+
+  message: any;
+
+  constructor(public dialogRef: MdDialogRef<DialogResultExampleDialog>) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { AppComponent } from './app.component';
+import {AppComponent, DialogResultExampleDialog} from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogResultExampleDialog
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [
+    DialogResultExampleDialog
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
